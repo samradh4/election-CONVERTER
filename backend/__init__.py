fdb081aa8f847f1696e40cfe3f2a8beaf3609d4e
samradh4 <samradhjoshi4@gmail.com>
2026-08-07 04:25:41 +0530

    Enable targeted missing-EPIC repair in production

--- backend/__init__.py
+++ backend/__init__.py
@@ -9,3 +9,7 @@ _apply_strict_name_patch()
 from .legacy_epic_patch import apply as _apply_legacy_epic_patch
 
 _apply_legacy_epic_patch()
+
+from .strict_epic_repair import apply as _apply_strict_epic_repair
+
+_apply_strict_epic_repair()
