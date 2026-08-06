ebe05b7656e7b9691c0eb120ce315f02e3229b9f
samradh4 <samradhjoshi4@gmail.com>
2026-08-07 04:20:51 +0530

    Enable selective Hindi name cleanup in production

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,3 +1,7 @@
 from .accuracy_patch import apply as _apply_accuracy_patch
 
 _apply_accuracy_patch()
+
+from .strict_name_patch import apply as _apply_strict_name_patch
+
+_apply_strict_name_patch()
